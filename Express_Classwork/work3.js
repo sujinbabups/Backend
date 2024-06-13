098c127587fd5f6b5105bd49b357be7c61b1d4f4
sujinbabups <sujinbu85@gmail.com>
2024-06-13 14:28:52 +0530

    commit2

--- Express_Classwork/work3.js
+++ Express_Classwork/work3.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 
@@ -9,15 +7,11 @@ app.use(express.json());
 app.post('/reviews', (req, res) => {
   const { bookTitle, reviewContent } = req.body;
 
- 
-  if (!bookTitle || !reviewContent) {
-    return res.status(400).json({ error: 'Book title and review content are required.' });
-  }
 
-  res.status(201).json({ message: 'Review submitted successfully.', review: { bookTitle, reviewContent } });
+  res.status(201).json({ "review status": 'Review submitted successfully.', review: { bookTitle, reviewContent } });
 });
 
-const PORT = 3000;
+const PORT = 3005;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
