4f34b605b2c973037b3376821e27a54f9e630ab6
sujinbabups <sujinbu85@gmail.com>
2024-06-20 20:41:14 +0530

    portfolio

---
+++ Express_Assignment/Portfolio/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const path= require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+port=3000;
+app.listen(port)
+console.log(`the server is running in ${port}`);
